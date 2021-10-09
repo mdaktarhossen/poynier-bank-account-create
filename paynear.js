e71a23b933bfb3aa2a584f374579d5f180b96cf6
mdaktarhossene <mdaktarhossen20@gmail.com>
2021-10-09 23:03:14 +0600

    this is a paynear bank Account

--- paynear.js
+++ paynear.js
@@ -1,27 +1,3 @@
-// // login area event handel
-// const button=document.getElementById("btn");
-// button.addEventListener('click', function(){
-//   const loginPage=document.getElementById('login');
-//   loginPage.style.display='none';
-
-//   const secondPage=document.getElementById('second-page');
-//   secondPage.style.display='block';
-// })
-// // second page event handle
-// const depositeButton=document.getElementById('depositeButton');
-// depositeButton.addEventListener('click', function(){
-
-//   const depositevalu=document.getElementById('depositevalu').value;
-//   const depositeNumber=parseFloat(depositevalu);
-
-//   const depositeAmount=document.getElementById('depositeAmount').innerText;
-//   const depositeAmountNumber=parseFloat(depositeAmount)
-//   const totalDeposite=depositeNumber+depositeAmountNumber;
-
-//   document.getElementById('depositeAmount').innerText=totalDeposite;
-//   document.getElementById('depositevalu').value='';
-
-// })
 
 // FIRST PAGE EVENT HANDEL ADD
 const button = document.getElementById("btn");
