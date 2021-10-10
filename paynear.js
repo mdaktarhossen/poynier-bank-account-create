93598f55df34070ba5b2ca3fe0d92823b6cf99c0
mdaktarhossene <mdaktarhossen20@gmail.com>
2021-10-10 16:50:15 +0600

    Paynear Bank Account

--- paynear.js
+++ paynear.js
@@ -10,22 +10,28 @@ blockArea.style.display='block'
 
 // second page event handale add
 const depositeBtn=document.getElementById('depositeBtn')
-depositeBtn.addEventListener('click', function(){
+  depositeBtn.addEventListener('click', function(){
   const depositeAmountNumber=getInputNumber('deposite-Amount');
-
   spanTagUpdated('current-Deposite' ,depositeAmountNumber)
   spanTagUpdated('current-Balance' ,depositeAmountNumber)
   document.getElementById('deposite-Amount').value="";
 })
+
 const WithdrowButton=document.getElementById('WithdrowBtn');
-WithdrowButton.addEventListener('click', function(){
-const WithdrowAmountNumber=getInputNumber('Withdrow-Amount')
+  WithdrowButton.addEventListener('click', function(){
+  const WithdrowAmountNumber=getInputNumber('Withdrow-Amount');
+  spanTagUpdated('current-withdrow' ,WithdrowAmountNumber);
+  spanTagUpdated('current-Balance' ,-1*WithdrowAmountNumber);
+  document.getElementById('Withdrow-Amount').value=''
+
 })
+
 function getInputNumber(id){
   const Amount=document.getElementById(id).value;
-const mountNumber=parseFloat(Amount);
-return AmountNumber;
+  const AmountNumber=parseFloat(Amount);
+  return AmountNumber;
 }
+
 function spanTagUpdated(id, depositeAmountNumber){
   const current=document.getElementById(id).innerText;
   const currentNumber=parseFloat(current);
